a3a58b66d4ab7743077716d22697722a3e2a6a5c
marcelog5 <marcelovictorg2@gmail.com>
2024-06-09 10:49:42 -0300

    Apartment less anemic

---
+++ Bookify/Domain/Apartments/Currency.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Apartments
+{
+    public record Currency
+    {
+        internal static readonly Currency None = new(string.Empty);
+        public static readonly Currency Brl = new("BRL");
+        public static readonly Currency Usd = new("USD");
+        public static readonly Currency Eur = new("EUR");
+
+        private Currency(string code) => Code = code;
+
+        public string Code { get; init; }
+
+        public static Currency FromCode(string code)
+        {
+            return All.FirstOrDefault(x => x.Code == code) ??
+                throw new ApplicationException($"The currency code is invalid");
+        }
+
+        public static readonly IReadOnlyCollection<Currency> All = new[] 
+        { 
+            Brl, 
+            Usd, 
+            Eur 
+        };
+    }
+}
---
+++ Bookify/Domain/Apartments/Money.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Apartments
+{
+    public record Money(decimal Amount, Currency Currency)
+    {
+        public static Money operator +(Money m1, Money m2)
+        {
+            if (m1.Currency != m2.Currency)
+            {
+                throw new ApplicationException("Cannot sum money with different currencies");
+            }
+
+            return new Money(m1.Amount + m2.Amount, m1.Currency);
+        }
+
+        public static Money Zero() => new Money(0, Currency.None);
+    }
+}
