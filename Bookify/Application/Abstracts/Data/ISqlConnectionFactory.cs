55cf1c245e9d5e79bd5064be31892cf5e8b40e0b
marcelog5 <marcelovictorg2@gmail.com>
2024-06-14 00:15:05 -0300

    Created Booking Query flow

---
+++ Bookify/Application/Abstracts/Data/ISqlConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Application.Abstracts.Data
+{
+    public interface ISqlConnectionFactory
+    {
+        IDbConnection CreateConnection();
+    }
+}
---
+++ Bookify/Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Abstracts.Messaging;
+
+namespace Application.Bookings.GetBooking
+{
+    public sealed record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>;
+}
